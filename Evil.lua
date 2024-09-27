3a38c474912e69062ed3d42e0d37e2924fbbcbdc
Mathguy23 <165744246+Mathguy23@users.noreply.github.com>
2024-09-26 20:09:21 -0400

    polychrome blinds

--- Evil.lua
+++ Evil.lua
@@ -14,19 +14,25 @@ function set_blind_editions()
         Boss = {},
     }
     local small_rand = pseudorandom(pseudoseed('e_small'))
-    if small_rand < 0.15 then
+    if small_rand < 0.07 then
+        G.GAME.blind_edition["Small"].poly = true
+    elseif small_rand < 0.15 then
         G.GAME.blind_edition["Small"].holo = true
     elseif small_rand < 0.4 then
         G.GAME.blind_edition["Small"].foil = true
     end
     local big_rand = pseudorandom(pseudoseed('e_big'))
-    if big_rand < 0.2 then
+    if small_rand < 0.1 then
+        G.GAME.blind_edition["Big"].poly = true
+    elseif big_rand < 0.2 then
         G.GAME.blind_edition["Big"].holo = true
     elseif big_rand < 0.6 then
         G.GAME.blind_edition["Big"].foil = true
     end
     local boss_rand = pseudorandom(pseudoseed('e_boss'))
-    if boss_rand < 0.4 then
+    if small_rand < 0.2 then
+        G.GAME.blind_edition["Boss"].poly = true
+    elseif boss_rand < 0.4 then
         G.GAME.blind_edition["Boss"].holo = true
     elseif boss_rand < 0.7 then
         G.GAME.blind_edition["Boss"].foil = true
